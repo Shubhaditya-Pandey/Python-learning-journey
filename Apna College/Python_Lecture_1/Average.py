c5a7dfa55dcae3bf344ab3ebaf311c42e30c8a83
Shubh <shubhadityapandey46@gmail.com>
2026-06-25 19:49:31 +0530

    Delete Apna College directory

--- Apna College/Python_Lecture_1/Average.py
+++
@@ -1,4 +0,0 @@
-a = float(input("Enter any no. :"))
-b = float(input("Enter any no. :"))
-average=(a+b)/2
-print("Average of two no. ",average)
--- Apna College/Python_Lecture_1/Sum.py
+++
@@ -1,5 +0,0 @@
-# Sum of two no.
-a = int(input("Enter any no: "))
-b = int(input("Enter any no:"))
-sum = a+b
-print(sum)
